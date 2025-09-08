b338c394e684c6294883c3ee5599c6376ce7a09f
kgben08 <familyfirst04250108@gmail.com>
2025-09-09 00:15:21 +0900

    feat: Add tag schema and functionality to posts

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import tag from './tag'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, tag, blockContent]
